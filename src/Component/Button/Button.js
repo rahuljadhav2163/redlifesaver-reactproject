2e810c21cb641628dd68e2be336a4bf3ef9253a6
rahuljadhav2163 <rgj4002@gmail.com>
2023-10-02 12:17:44 +0530

    added navbar

--- src/Component/Button/Button.js
+++ src/Component/Button/Button.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./Button.css"
-function Button() {
+function Button({text}) {
   return (
-    <div className='button'>Button</div>
+    <button className='btn-navbar'><i class="bi bi-person-plus-fill"></i> {text}  
+    </button>
   )
 }
 
 export default Button
+
+
+
+
+export  function Outbtn({btnname}) {
+  return (
+    <button className='btn-navbar-out'><i class="bi bi-unlock"></i> {btnname}  
+    </button>
+  )
+}
+
+export function FindBlood({named}) {
+  return (
+    <button className='btn-findblood'> {named}  
+    </button>
+  )
+}
