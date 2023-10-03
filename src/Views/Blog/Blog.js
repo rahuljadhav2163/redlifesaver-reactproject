98ddc27d496a4fa6f91c589ae9291063335902ad
mansidethe <mansidethe18@gmail.com>
2023-10-03 08:56:38 +0530

    added blogs

--- src/Views/Blog/Blog.js
+++ src/Views/Blog/Blog.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import "./Blog.css"
-function Blog() {
+import Blog from "./blog.json"
+import PreviewBlogCard from './../../Component/PreviewBlogCard/PreviewBlogCard'
+
+function Blogs() {
   return (
-    <div className='blog'>Blog</div>
+      <div >
+          <h1 className='blog'>Blogs</h1>
+<div className='blog-container'>
+          {
+              Blog.map((post, index) => {
+                return<PreviewBlogCard key={index} id={post.id} title={post.title} description={post.description}/>
+              })
+
+          }
+</div>
+      </div>
+
   )
 }
 
-export default Blog
+export default Blogs
