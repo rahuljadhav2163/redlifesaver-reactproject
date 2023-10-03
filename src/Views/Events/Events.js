f8d9589794526b871a6e3c0b42cb0818d46268e3
shwetapund <pundshweta2020@gmail.com>
2023-10-03 16:08:07 -0700

    Pune city events is ready

--- src/Views/Events/Events.js
+++ src/Views/Events/Events.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Events.css"
 import Navbar from './../../Component/Navbar/Navbar'
 import Config from './Events.json';
 
 
 function Events() {
+  
+
   return (
     <>
       <Navbar />
@@ -22,10 +24,10 @@ function Events() {
         <div className='city'>
           <h1 className='city-title'>City</h1>
 
-          <h2>Pune</h2>
-          <h2>Nagpur</h2>
-          <h2>Dhule</h2>
-          <h2>Mumbai</h2>
+          <h2 className='cityName'>Pune</h2>
+          <h2 className='cityName'>Nagpur</h2>
+          <h2 className='cityName'>Dhule</h2>
+          <h2 className='cityName'>Mumbai</h2>
         </div>
 
         <div className='event'>
@@ -34,10 +36,10 @@ function Events() {
 
           {
             Config.events.map((eventDeta, index) => {
-              const { img, title, description, date,imgEvent, imgEventnew1, dateEventNew,titleEvent1} = eventDeta;
+              const { img, title, description, date,imgEvent, imgEventnew1, dateEventNew,titleEvent1,descriptionEvent1,button} = eventDeta;
 
               return (<>
-              <div className='activity'>
+              {/* <div className='activity'>
                   <div>
                     <img src={imgEventnew1} height={190} width={230} style={{boxShadow:"0 0 5px rgb(128, 127, 127)"}}/>
                
@@ -45,19 +47,20 @@ function Events() {
                   <div className='title-description'>
                     <h3>{titleEvent1}</h3>
                     <b>{dateEventNew}</b>
-                    <p>{description}</p>
+                    <p className='description'>{descriptionEvent1}</p>
                   </div>
-                </div>
+                </div> */}
 
                 <div className='activity'>
                   <div>
                     <img src={imgEvent} height={190} width={230} />
                  
-                  </div>
+                  </div>/
                   <div className='title-description'>
-                    <h3>{title}</h3>
+                   <h3>{title} <span className='btn-new'>{button}</span></h3>
                     <b>{date}</b>
                     <p>{description}</p>
+                   
                   </div>
                 </div>
                 <br/>
