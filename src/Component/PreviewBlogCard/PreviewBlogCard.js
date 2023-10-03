8270df73952715e56b1fe7e5c57b76d4f818eaf6
mansidethe <mansidethe18@gmail.com>
2023-10-03 23:19:40 +0530

    add images and data

--- src/Component/PreviewBlogCard/PreviewBlogCard.js
+++ src/Component/PreviewBlogCard/PreviewBlogCard.js
@@ -2,16 +2,17 @@ import React from "react";
 import "./PreviewBlogCard.css"
 import { Link } from 'react-router-dom'
 
-function PreviewBlogCard({id, title, description}){
+function PreviewBlogCard({id, title, date,image}){
     return(
         <div className="main-component">
         <div className="preview-blog-card">
                         
-        <h1>{id}</h1>
+       
+        <img src={image} className="donor-image"/>
     <h1 className='card-title'>{title}</h1>
     
-    <h2 className='card-text'> {description}</h2>
-    <Link to={`/post/read/${id}`}>Read More</Link>
+    <h2 className='card-text'> {date}</h2>
+    <Link to={`/blog ${id}`}>Read More</Link>
     </div>
     </div>
     )
