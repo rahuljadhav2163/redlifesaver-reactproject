785924d82cd0af97e29ebc0eff53c7b65b8d8dbb
rahuljadhav2163 <rgj4002@gmail.com>
2023-10-03 12:27:53 +0530

    added respon=sines

--- src/Component/Button/Button.js
+++ src/Component/Button/Button.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./Button.css"
-function Button({text}) {
+function Button({ text }) {
   return (
-    <button className='btn-navbar'><i class="bi bi-person-plus-fill"></i> {text}  
+    <button className='btn-navbar'><i class="bi bi-person-plus-fill"></i> {text}
     </button>
   )
 }
@@ -12,23 +12,30 @@ export default Button
 
 
 
-export  function Outbtn({btnname}) {
+export function Outbtn({ btnname }) {
   return (
-    <button className='btn-navbar-out'><i class="bi bi-unlock"></i> {btnname}  
+    <button className='btn-navbar-out'><i class="bi bi-unlock"></i> {btnname}
     </button>
   )
 }
 
-export function FindBlood({named}) {
+export function FindBlood({ named }) {
   return (
-    <button className='btn-findblood'> {named}  
+    <button className='btn-findblood'> {named}
     </button>
   )
 }
 
-  export function SearchBtn({search}) {
-    return (
-      <button className='btn-search'><i class="bi bi-search-heart"></i> {search}  
-      </button>
-    )
+export function SearchBtn({ search }) {
+  return (
+    <button className='btn-search'><i class="bi bi-search-heart"></i> {search}
+    </button>
+  )
+}
+
+export function Cardbtn({ value }) {
+  return (
+    <button className='btncardout'>{value}
+    </button>
+  )
 }
