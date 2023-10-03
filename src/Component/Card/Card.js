6f34819d3ca7138bdf991db269e5b3b5305da684
rahuljadhav2163 <rgj4002@gmail.com>
2023-10-03 22:49:04 +0530

    added map function for card count

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import "./Card.css"
-function Card() {
-  return (
-    <div className='card'>Card</div>
-  )
-}
+import React from "react";
+import "./Card.css";
 
-export default Card
+export  const Countcard = (props) => {
+  const {number , group} = props;
+
+  return (
+    <div className="count-card">
+      
+      
+      <h1 className="numer-count">{group}</h1>
+      <h3 className="numer-count num">{number}</h3>
+    </div>
+  );
+};
+
--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
 import Navbar from "./../../Component/Navbar/Navbar"
 import { Cardbtn, FindBlood } from './../../Component/Button/Button'
-import { Outbtn } from './../../Component/Button/Button'
+import { Countcard } from '../../Component/Card/Card'
+import Datacount from "./Home.json"
+import Donateblood from './img/donate-blood-haert.jpg'
 function Home() {
+
+
+  const [bloodcount, setBloodCount] = useState(Datacount);
+
+
+
   return (
     <div>
 
       <div className='background-img'>
-        <Navbar/>
+        <Navbar />
         <div className='container-blood-find'>
           <div className='homeform-container'>
             <select className="selectgroup">
@@ -48,41 +56,57 @@ function Home() {
 
           <div className='card-home'>
             <p className='name-danation'>Donation</p>
-           <div className='container-donation'>
-             <p className='rupees'><i class="bi bi-currency-rupee"></i></p>
-             <p> <span className='danate-line'>We are graetfull for our real hero and hope for them always success who help danate.<Cardbtn value="Danate Us"/></span></p>
-          
-           </div>
-           
-           
+            <div className='container-donation'>
+              <p className='rupees'><i class="bi bi-currency-rupee"></i></p>
+              <p> <span className='danate-line'>We are graetfull for our real hero and hope for them always success who help danate.<Cardbtn value="Danate Us" /></span></p>
+
+            </div>
+
+
           </div>
-           
+
           <div className='card-home'>
             <p className='name-danation'>Volunteer</p>
             <div className='container-donation'>
-             <p className='rupees'><i class="bi bi-person-heart"></i></p>
-             <p> <span className='danate-line'>You will become a vital part of biggest blood donation organization Red Life Saver team in.<Cardbtn value="Joinn Now"/></span></p>
-           </div>
+              <p className='rupees'><i class="bi bi-person-heart"></i></p>
+              <p> <span className='danate-line'>You will become a vital part of biggest blood donation organization Red Life Saver team in.<Cardbtn value="Joinn Now" /></span></p>
+            </div>
           </div>
-          
 
 
-          <div className='card-home'>
+
+          <div className='card-home homii'>
             <p className='name-danation'>Causes</p>
             <div className='container-donation'>
-             <p className='flag'><i class="bi bi-flag-fill"></i></p>
-             <p> <span className='danate-line'>Red life saver team recieve today recent causes to fundering for capable charitable project.<span className='btnnnn'><Cardbtn value="Causes"/></span></span></p>
-           </div>
-           
+              <p className='flag'><i class="bi bi-flag-fill"></i></p>
+              <p> <span className='danate-line'>Red life saver team recieve today recent causes to fundering for capable charitable project.<span className='btnnnn'><Cardbtn value="Causes" /></span></span></p>
+            </div>
+
           </div>
 
 
         </div>
-      </div>
-      <div className='gggg'>
+        <p className='count-line'>Total Blood Donar Register With Red Life Saver Team.</p>
+        <div className='countcontainer'>
+          {
+
+            bloodcount.map((countInfo)=>{
+              const {number,group}=countInfo;
+              return (
+                <Countcard number={number} group={group} />
+              )
+            })
+          }
+        </div>
+
+          <div className='containrdonater'>
+          <img className='donateblood-img'  src={Donateblood} />
+          </div>
 
       </div>
+
     </div>
+
 
   )
 }
